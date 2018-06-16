982e9ad79bea3b36f6a94d48fc03cf9600e4b9e2
Tam Vu <vu.t@husky.neu.edu>
2018-06-15 23:25:18 -0400

    clean

--- src/services/GroupService.js
+++ src/services/GroupService.js
@@ -6,15 +6,14 @@ const GROUP_USER_URL = LOCAL_URL + '/api/user/UID/group'
 const GROUP_USERROLE_URL = LOCAL_URL + '/api/user/UID/USER_ROLE/group'
 
 
-export default
-class GroupService {
+export default class GroupService {
     constructor(singletonToken) {
         if (_singleton !== singletonToken)
             throw new Error('Cannot instantiate directly.');
     }
 
     static get instance() {
-        if(!this[_singleton])
+        if (!this[_singleton])
             this[_singleton] = new GroupService(_singleton);
         return this[_singleton]
     }
@@ -22,21 +21,21 @@ class GroupService {
 
     findAllGroupsForUser(uid) {
         return fetch(GROUP_USER_URL.replace('UID', uid))
-            .then(function(response){
+            .then(function (response) {
                 return response.json();
             });
     }
 
     findAllGroupsByUserRole(userRole, uid) {
         return fetch(GROUP_USERROLE_URL.replace('UID', uid).replace('USER_ROLE', userRole))
-            .then(function(response){
+            .then(function (response) {
                 return response.json();
             });
     }
 
     findGroupByID(id) {
         return fetch(GROUP_API_URL + '/' + id)
-            .then(function(response){
+            .then(function (response) {
                 return response.json();
             });
     }
@@ -57,11 +56,12 @@ class GroupService {
         return fetch(GROUP_API_URL + '/' + id, {
             method: 'DELETE'
         })
+    }
 
 
     findAllMembersForGroup(groupId) {
         return fetch('http:localhost:8080/api/group/' + groupId + '/members')
-            .then(function(response) {
+            .then(function (response) {
                 return response.json();
             })
     }
@@ -69,13 +69,12 @@ class GroupService {
     updateMembers(groupId, members) {
         return fetch('http:localhost:8080/api/group/' + groupId + '/members',
             {
-                body:JSON.stringify(members),
+                body: JSON.stringify(members),
                 headers: {'Content-Type': 'application/json'},
                 method: 'PUT'
             })
-            .then(function(response) {
+            .then(function (response) {
                 return response.json();
             })
     }
 }
-    }
