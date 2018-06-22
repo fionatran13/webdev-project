560dcb78caff2c5c727e4c450d624398813d4c00
Tam Vu <vu.t@husky.neu.edu>
2018-06-22 15:50:43 -0400

    button

--- src/index.js
+++ src/index.js
@@ -5,14 +5,30 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import GroupPage from './containers/GroupPage';
 import UserProfile from "./containers/UserProfile";
+import FacebookLogin from 'react-facebook-login';
+
+const responseFacebook = (response) => {
+    console.log(response);
+}
+
+const componentClicked = (event) => {
+    console.log(event);
+}
 
 ReactDOM.render(
     <Router>
-        <div>
-            <Route path="/user/:userId"
-                   component={UserProfile}>
-            </Route>
-        </div>
+        {/*<FacebookLogin*/}
+            {/*appId="2260374724192830"*/}
+            {/*autoLoad={false}*/}
+            {/*fields="name,email,picture"*/}
+            {/*onClick={componentClicked}*/}
+            {/*callback={responseFacebook} />*/}
+
+        {/*<div>*/}
+            {/*<Route path="/user/:userId"*/}
+                   {/*component={UserProfile}>*/}
+            {/*</Route>*/}
+        {/*</div>*/}
     </Router>,
     document.getElementById('root')
 )
