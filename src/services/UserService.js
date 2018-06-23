2e3dfb6d90fa831b616336772010e297349a5e2d
fionatran <tran.f@northeastern.edu>
2018-06-23 13:05:55 -0400

    created user service with create user method

---
+++ src/services/UserService.js
@@ -0,0 +1,26 @@
+let _singleton = Symbol();
+
+export default class UserService {
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken)
+            throw new Error('Cannot instantiate directly.');
+    }
+
+    static get instance() {
+        if (!this[_singleton])
+            this[_singleton] = new UserService(_singleton);
+        return this[_singleton]
+    }
+
+    createUser(user) {
+        return fetch('http:localhost:8080/api/user', {
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        }).then(function (response) {
+            return response.json();
+        })
+    }
+}
