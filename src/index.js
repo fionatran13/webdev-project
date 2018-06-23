eb672d8e20bf54d610f48e965025727380b0e991
Tam Vu <vu.t@husky.neu.edu>
2018-06-22 21:27:58 -0400

    get friends

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* global FB */
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import ReactDOM from 'react-dom';
@@ -7,12 +8,39 @@ import GroupPage from './containers/GroupPage';
 import UserProfile from "./containers/UserProfile";
 import FacebookLogin from 'react-facebook-login';
 
+
+export const TAM_ACCESS_TOKEN =
+    "EAAdxd9JUTZBoBAFvS12SoIH1bXo2xHPZBUnDMtEFZAnJtfwYqVAbIEgoS2sstDZAZAoHLIU1tOVDS2QkWVaYCUzFJRMp3p1VN1SNll2seTZA98TqNuuiXPD5zWNuW9aP326LOBCmEyzpyMmlZCNOSCj7wsevp7TeRj8Pk8afAN2JfMwCoZAJXgDU2lJhY5WDWTaFEGb0kBo4lAZDZD"
+
 const responseFacebook = (response) => {
     console.log(response);
+
+    FB.api(
+        '/' + response.id,
+        'GET',
+        {},
+        function(response) {
+            // Insert your code here
+            console.log(response)
+        }
+    );
+
+    FB.Event.subscribe('auth.authResponseChange', async response => {
+        console.log(response)
+        // try {
+        //     const { profile, myFriends } = await getData();
+        //     this.setState({ status: response.status, profile, myFriends });
+        // } catch (e) {
+        //     this.setState({ status: 'err' });
+        // }
+    });
 }
 
 const componentClicked = (event) => {
-    console.log(event);
+    // FB.login(function(response) {
+    //     console.log(response)
+    // }, {scope: ['user_friends']});
+
 }
 
 ReactDOM.render(
@@ -20,7 +48,7 @@ ReactDOM.render(
         <FacebookLogin
             appId="2260374724192830"
             autoLoad={false}
-            fields="name,email,picture"
+            fields="name,email,picture,friends"
             onClick={componentClicked}
             callback={responseFacebook} />
 
